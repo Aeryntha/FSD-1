c5be9657b35d98da66ee93ce643c7ca2af22d8c0
Tirath Shrimali <shrimalitirath@gmail.com>
2026-09-16 23:09:39 +0530

    organized everything

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import TaskCard from './components/TaskCard';
+import TaskList from './components/TaskList';
 import TaskApp from './components/TaskApp';
 
 function App() {
-  return <TaskApp />;
+  return (
+    <div>
+      <h1>React Exercises</h1>
+
+      <h2>Task Card</h2>
+      <TaskCard title="Finish assignment" priority="high">
+        <p>Due tomorrow</p>
+      </TaskCard>
+
+      <h2>Task List</h2>
+      <TaskList />
+
+      <h2>Advanced Task App</h2>
+      <TaskApp />
+    </div>
+  );
 }
 
 export default App;
