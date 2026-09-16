3bddcecd4963a878a2dcfe8c631abe8effa6399c
Tirath Shrimali <shrimalitirath@gmail.com>
2026-09-16 22:59:53 +0530

    Add react router task routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,40 @@
-import TaskList from './components/TaskList';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  useParams,
+} from 'react-router-dom';
+
+function TaskDetail() {
+  const { id } = useParams();
+
+  return <h2>Viewing task #{id}</h2>;
+}
+
+function Home() {
+  return <h2>Home Page</h2>;
+}
+
+function TaskList() {
+  return <h2>Task List</h2>;
+}
 
 function App() {
   return (
-    <div>
-      <h1>My Task List</h1>
-      <TaskList />
-    </div>
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link> |{' '}
+        <Link to="/tasks">Tasks</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/tasks" element={<TaskList />} />
+        <Route path="/tasks/:id" element={<TaskDetail />} />
+        <Route path="*" element={<h2>404 - Not Found</h2>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
